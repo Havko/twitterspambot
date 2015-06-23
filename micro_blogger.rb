6d5731ccde7cabb5eb2c3a0c10399caf41134966
Steven Havko <stevenhavko@gmail.com>
2015-06-23 14:27:36 -0400

    Tutorial finished

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,4 +1,5 @@
 require 'jumpstart_auth'
+require 'bitly'
 class MicroBlogger
 	attr_reader :client
 
@@ -28,6 +29,8 @@ class MicroBlogger
 			  when 'dm' then dm(parts[1], parts[2..-1].join(" ")) 
 			  when 's' then spam_my_followers(parts[1..-1].join(" "))
 			  when 'elt' then everyones_last_tweet
+			  when 's' then shorten(parts[1])
+			  when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
 			  else
 			  	puts "Sorry, I don't know how to #{command}"
 			  end
@@ -60,14 +63,26 @@ class MicroBlogger
 		end
 
 	def everyones_last_tweet
-		friends = @client.friends
-		friends.each do |friend|
-          puts friend.status.source
-          puts ""
-		end
-	end
+    friends = @client.friends
+    friends = friends.map { |friend| @client.user(friend) }
+    friends.sort_by! { |friend| friend.screen_name.downcase}
+   
+    friends.each do |friend|
+      timestamp = friend.status.created_at.strftime('%A, %b %d')
+      tweet = friend.status.text
+      puts "@#{friend.screen_name} said..."
+      printf "#{tweet} at #{timestamp}"
+      puts ''
+    end
+  end
 
-
+  def shorten(original_url)
+    bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    puts "Shortening this url: #{original_url}"
+    return bitly.shorten(original_url).short_url
+    
+   end
+    
 
 end
 mytweet = "".ljust(140, "abcd")
